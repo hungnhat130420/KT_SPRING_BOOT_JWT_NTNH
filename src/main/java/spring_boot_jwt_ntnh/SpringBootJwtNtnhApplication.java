f5fa54d2771b35fb02ac017da740fcca8e4f4b27
hungnhat130420 <hungnguyen.130420@gmail.com>
2021-09-22 08:38:12 +0700

    master branch : entity and configuaration

---
+++ src/main/java/spring_boot_jwt_ntnh/SpringBootJwtNtnhApplication.java
@@ -0,0 +1,13 @@
+package spring_boot_jwt_ntnh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtNtnhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtNtnhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring_boot_jwt_ntnh/SpringBootJwtNtnhApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_jwt_ntnh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtNtnhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
